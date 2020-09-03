72c05fd0cc3e51b4094c3038f4b426d9b7ddad09
brunolira1 <69700129+brunolira1@users.noreply.github.com>
2020-09-02 21:33:53 -0300

    Update agente.py

--- agente.py
+++ agente.py
@@ -2,16 +2,14 @@ from turtle import *
 
 class Agente:
 
- def __init__(self, x, y, cor, tam_celula, tam_agente):
-    self._x = x
-    self._y = y
-    self._cor = cor
+ def __init__(self, tam_celula, tam_agente):
     self._tam_celula = tam_celula
     self._tam_agente = tam_agente
 
- def desenhar_agente(self):
+ def desenhar_agente(self, x, y, cor):
      c = self._tam_celula // 2
      up()
      goto(x + c, y + c)
      down()
      dot(self._tam_agente, cor)
+     
