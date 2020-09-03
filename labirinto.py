c8a9b35ccb7502e69561fcca91e96867df1fe7dc
brunolira1 <69700129+brunolira1@users.noreply.github.com>
2020-09-02 21:28:05 -0300

    Update labirinto.py

--- labirinto.py
+++ labirinto.py
@@ -7,13 +7,12 @@ class Labirinto:
  def obter_visinhos(lin, col):
    pass
 
- def __init__(self, dim, tam_celula, cor_celula, cor_pastilhas, x, y):
+ def __init__(self, dim, tam_celula):
   self._dim = dim
-  self._cor_celula = cor_celula
-  self._cor_pastilhas = cor_pastilhas
   self._tam_celula = tam_celula
-  self._x = x
-  self._y = y
+  self._matriz = np.random.randint(2, size=(dim,dim))
+
+ def ler_matriz_fixa(self):
   return [[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
             [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
             [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
@@ -35,7 +34,7 @@ class Labirinto:
             [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0],\
             [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0]]
 
- def ler_matriz_aleatoria(self):
+ def ler_matriz_aleatoria(self, dim):
   return np.random.randint(2,size=(self._dim,self._dim))
 
  def criar_labirinto(self, p1=500, p2=500, p3=370, p4=0):
@@ -45,13 +44,13 @@ class Labirinto:
   setup(p1,p2,p3,p4)
   for lin in range(self._dim):
    for col in range(self._dim):
-    if (matriz[lin][col] == 1):
-     x,y = em_coord_turtle()
-     desenhar_celula()
-     desenhar_pastilha()
+    if (self._matriz[lin][col] == 1):
+     x,y = self.em_coord_turtle(lin,col)
+     self.desenhar_celula(x, y, 'blue')
+     self.desenhar_pastilha(x, y, 'white')
 
- def desenhar_celula(self):
-  color(self._cor_celula)
+ def desenhar_celula(self, x, y, cor):
+  color(cor)
   up()
   goto(x,y)
   down()
@@ -62,36 +61,36 @@ class Labirinto:
   end_fill()
   up()
 
- def chao_da_celula(self):
-  chao_x = int(floor(self._x, self._tam_celula))
-  chao_y = int(floor(self._y, self._tam_celula))
+ def chao_da_celula(self, x, y):
+  chao_x = int(floor(x, self._tam_celula))
+  chao_y = int(floor(y, self._tam_celula))
   return chao_x, chao_y
 
- def em_coord_turtle(self):
+ def em_coord_turtle(self, lin, col):
   meio = self._dim // 2
   x = (col - meio) * self._tam_celula
   y = (meio - lin) * self._tam_celula
   return x, y
 
- def em_coord_matriz(self):
-  chao_da_celula()
+ def em_coord_matriz(self, x, y):
+  x, y = self.chao_da_celula(x,y)
   meio = self._dim // 2
-  self._lin = int(meio - (chao_y / tam_celula))
-  self._col = int(meio + (chao_x / tam_celula))
-  return self._lin, self._col
+  lin = int(meio - (y / tam_celula))
+  col = int(meio + (x / tam_celula))
+  return lin, col
 
- def cel_aleatoria(self):
+ def cel_aleatoria(self, lin, col):
   i, j = np.random.randint(self._dim, size=(2))
-  while(not eh_caminho()):
+  while(not self.eh_caminho(i, j)):
    i, j = np.random.randint(self._dim, size=(2))
   return i, j
 
- def eh_caminho():
-     return matriz [i][j] == 1
+ def eh_caminho(self):
+     return self.matriz[lin][col] == 1
 
- def desenhar_pastilha(self):
+ def desenhar_pastilha(self, x, y, cor):
   c = self._tam_celula // 2
   up()
   goto(x + c, y + c)
   down()
-  dot(3, self._cor_pastilhas)
+  dot(3, cor)
